fe0a10fe0f994bb4043868a021ec6b10dae63dca
neha123-4 <23115678@hitdoon.com>
2025-04-07 06:20:33 +0000

    datatype and var

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=14453
+let accountEmail="hitesh@gmail.com"
+var accountPassword="jaipur"
+accountCity="jaipur"
+// accountId=2
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword,accountCity])
+let accountState;
